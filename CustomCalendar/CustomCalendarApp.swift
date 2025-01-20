9f454d02b309a3dbfc4f67d103df4895af1abc9a
SyedAmmarSohail <ammarsohail321@gmail.com>
2025-01-20 17:52:15 +0500

    Initial Commit

---
+++ CustomCalendar/CustomCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalendarApp.swift
+//  CustomCalendar
+//
+//  Created by Syed Ammar on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CustomCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
